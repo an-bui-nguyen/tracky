ccc998c828f474dc19fd8016e81dbd997f0a02cb
Bùi Nguyễn Thúy An <66785692+an-bui-nguyen@users.noreply.github.com>
2024-01-21 23:53:50 +1000

    Align elements on landing page. Add login form. Adjust colors for buttons.

---
+++ src/services/login.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const baseUrl = 'http://localhost:3000'
+
+const login = async (username, password) => {
+  const response = await axios.post(baseUrl + "/api/login", { username, password });
+  // get jwt cookie from response data
+  
+  return response.data;
+}
+
+export default { login }
